dd81c567b17832e9fef19114014e0a635e513bba
Dr. Torge Szczepanek <t.szczepanek@cygnusnetworks.de>
2014-03-26 22:05:19 +0100

    Add Debian Packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+setup(name='gsmsapi',
+	version='0.10',
+	description='SMS API for (german) SMS providers',
+	author='Torge Szczepanek',
+	author_email='info@cygnusnetworks.de',
+	maintainer='Torge Szczepanek',
+	maintainer_email='info@cygnusnetworks.de',
+	license='MIT',
+	packages=['gsmsapi'],
+	#test_suite='discover_tests',
+)
