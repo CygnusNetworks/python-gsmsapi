71fb2fc819c82e2db4075c6e5e32b2addc99c63a
Dr. Torge Szczepanek <t.szczepanek@cygnusnetworks.de>
2014-05-21 11:42:11 +0200

    Add platforms and classifiers

--- setup.py
+++ setup.py
@@ -15,4 +15,12 @@ setup(name='gsmsapi',
 	url = 'https://github.com/CygnusNetworks/python-gsmsapi',
 	download_url = 'https://github.com/CygnusNetworks/python-gsmsapi/tarball/v0.10',
 	keywords = ["sms", "german", "sipgate", "smstrade", "api"],
+	platforms='any',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Software Development :: Libraries :: Python Modules']  # see: https://pypi.python.org/pypi?%3Aaction=list_classifiers
 )
